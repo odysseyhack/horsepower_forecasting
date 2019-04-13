e9cee555351a04cd1d72f1a4cf42efcbb2442338
karthick <karthickphysics@gmail.com>
2019-04-13 21:39:56 +0200

    further refactored code

--- src/train.py
+++ src/train.py
@@ -32,27 +32,22 @@ def split_data(df, ratio=0.7):
     X, y = df[:int(len(df)*ratio)], df[int(len(df)*ratio):]
     return pd.DataFrame(X), pd.DataFrame(y)
 
-def scale_data(train, test, scaler_pathprefix):
+def scale_data(train, test):
     scaler = MinMaxScaler(feature_range=(0, 1))
     train = scaler.fit_transform(train)
     test = scaler.transform(test)
+    return pd.DataFrame(train), pd.DataFrame(test)
 
-    joblib.dump(scaler, f'{scaler_pathprefix}_scaler.pkl')
-
-    train = pd.DataFrame(train)
-    test = pd.DataFrame(test)
-    return train, test
-
-def get_in_out_tgtidx(in_start, n_input, n_out):
-    in_end = in_start + n_input
-    out_end = in_end + n_out
+def get_in_out_tgtidx(in_start):
+    in_end = in_start + 120
+    out_end = in_end + 1
     return in_end, out_end, 0
 
 # convert history into inputs and outputs
-def to_supervised(data_arr, n_input=48, n_out=24):
+def to_supervised(data_arr,):
     X, y, in_start = list(), list(), 0
     for _ in range(len(data_arr)):
-        in_end, out_end, target_index = get_in_out_tgtidx(in_start, n_input, n_out)
+        in_end, out_end, target_index = get_in_out_tgtidx(in_start)
         if out_end < len(data_arr):  X.append(data_arr[in_start:in_end, :]); y.append(data_arr[in_end:out_end, target_index]); in_start += 1
     return np.array(X), np.array(y)
 
@@ -63,12 +58,7 @@ def drop_nans_from_data(X, y):
     temp_df = pd.merge(X, y, left_index=True, right_index=True)
     temp_df.dropna(how='any', inplace=True)
 
-    X = temp_df[train_cols]
-    y = temp_df[target_cols]
-
-    del temp_df
-
-    return X, y
+    return temp_df[train_cols], temp_df[target_cols]
 
 def convert_to_df(data_frame, history, forecast):
     X, y = to_supervised(data_frame.values, history, forecast)
@@ -86,12 +76,10 @@ def build_model(n_timesteps, n_outputs):
     model.add(TimeDistributed(Dense(1)))
     return model
 
-def reshape_data(X_train, X_test, y_train, y_test):
-    X_train = X_train.values.reshape(-1, HISTORY, 1)
-    X_test = X_test.values.reshape(-1, HISTORY, 1)
-    y_train = np.expand_dims(y_train, axis=2)
-    y_test = np.expand_dims(y_test, axis=2)
-    return X_train, X_test, y_train, y_test
+def reshape_data(X, y):
+    X = X.values.reshape(-1, HISTORY, 1)
+    y = np.expand_dims(y, axis=2)
+    return X, y
 
 if __name__ == "__main__":
     prefix = 'consumption'
@@ -102,7 +90,8 @@ if __name__ == "__main__":
 
     X_test, y_test = convert_to_df(test, history=HISTORY, forecast=FORECAST)
     X_test, y_test = drop_nans_from_data(X_test, y_test)
-    X_train, X_test, y_train, y_test = reshape_data(X_train, X_test, y_train, y_test)
+    X_train, y_train = reshape_data(X_train, y_train)
+    X_test, y_test = reshape.data(X_test, y_test)
     n_timesteps, n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
     model = build_model(n_timesteps, n_features, n_outputs)
     model.compile(loss='mse', optimizer=Adam(lr=0.001, decay=1e-6, clipnorm=1.))
